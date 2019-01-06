ac4a1c3dbdfb5e354b24b9196cf6fe7f3ae1f3d9
Allan Amstadt <allan@amstadt.de>
2019-01-06 17:24:40 +0100

    Changes for Beta Release

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,11 +1,20 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
+@import AppCenter;
+@import AppCenterAnalytics;
+@import AppCenterCrashes;
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
+
+
+  [MSAppCenter start:@"1cdd05fd-1d26-45d7-8d5e-7020938d3e7e" withServices:@[
+  [MSAnalytics class],
+  [MSCrashes class]
+]];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
